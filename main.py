35a9b126cb7d28e9a01e297307a67db535694376
andeen171 <zeander21@outlook.com.br>
2021-04-23 18:43:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,60 @@
-import sys
+# import sys
 # import subprocess
 from pytube import YouTube
 import ffmpeg
+from pytube.exceptions import VideoUnavailable, LiveStreamError, RegexMatchError
+
+
+class PytubeDownloader:
+    resolutions = {'1': [299, 140],
+                   '2': [136, 140],
+                   '3': [135, 140],
+                   '4': [134, 140],
+                   '5': [133, 140],
+                   '6': [160, 140]}  # The resolution match by it number and itag for audio and video
+
+    def __init__(self, url, pathvideo='./downloads/video.mp4', pathaudio='./downloads/audio.mp4'):
+        try:
+            self.video = YouTube(url)
+            self.pathvideo = ffmpeg.input(pathvideo)
+            self.pathaudio = ffmpeg.input(pathaudio)
+        except VideoUnavailable:
+            print('This video is unavailable')
+            exit()
+        except LiveStreamError:
+            print("It's a livestream URL!")
+            exit()
+        except RegexMatchError:
+            print('Please enter a valid youtube video URL!')
+            exit()
+
+    def Summary(self):
+        print('Summary:')
+        print(f'Title: {self.video.title}')
+        print(f'Duration: {self.video.length / 60:.2f} minutes')
+        print(f'Rating: {self.video.rating:.2f}')
+        print(f'{self.video.views} views\n')
+        print('What resolution you want?')
+
+    def Download(self, command):
+        self.video.streams.get_by_itag(self.resolutions[command][0])\
+            .download(output_path='./downloads/', filename='video')
+        self.video.streams.get_by_itag(self.resolutions[command][1])\
+            .download(output_path='./downloads/', filename='audio')
+
+    def FFmpegConcat(self):
+        ffmpeg.concat(self.pathvideo, self.pathaudio, v=1, a=1).output(f'./downloads/{self.video.title}.mp4').run()
+        # Same FFmpeg process to join video and audio but using subprocess instead of ffmpeg-python
+        # subprocess.run(f"""ffmpeg -i self.pathvideo -i self.pathaudio
+        #              -c 'copy' './downloads/{self.video.title}.mp4'""")
+# ---------------------------------------------------------------------------------------------------------------------
+
 
 print('Paste the URL of the video:')
-command = input()
-resolutions = {'1': [160, 140],
-               '2': [160, 140],
-               '3': [160, 140],
-               '4': [160, 140],
-               '5': [160, 140],
-               '6': [160, 140]}
-
-video = YouTube(command)
-
-print('Summary:')
-print(f'Title: {video.title}')
-print(f'Duration: {video.length / 60:.2f} minutes')
-print(f'Rating: {video.rating:.2f}')
-print(f'{video.views} views\n')
-print('What resolution you want?')
-command = input('1 - 1080p, 2 - 720p, 3 - 480p, 4 - 360, 5 - 240p, 6 - 144p : ')
-video.streams.get_by_itag(resolutions[command][0]).download(output_path='./downloads/', filename='video')
-video.streams.get_by_itag(resolutions[command][1]).download(output_path='./downloads/', filename='audio')
-
-input_video = ffmpeg.input('./downloads/video.mp4')
-
-input_audio = ffmpeg.input('./downloads/audio.mp4')
-
-ffmpeg.concat(input_video, input_audio, v=1, a=1).output(f'./downloads/{video.title}.mp4').run()
-
-# videofile = "./downloads/video.mp4"
-# audiofile = "./downloads/audio.mp4"
-# outputfile = "./downloads/finished_video.mp4"
-# codec = "copy"
-# subprocess.run(f"ffmpeg -i {videofile} -i {audiofile} -c {codec} {outputfile}")
+# video = YouTube(input())
+# for i in video.streams.filter(file_extension='mp4'):
+#     print(i)
+run = PytubeDownloader(input())
+run.Summary()
+run.Download(input('1 - 1080p, 2 - 720p, 3 - 480p, 4 - 360, 5 - 240p, 6 - 144p : '))
+run.FFmpegConcat()
