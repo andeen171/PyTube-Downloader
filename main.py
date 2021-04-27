cb1820c1635f2e1721124e7ee239a761a6882fa8
andeen171 <zeander21@outlook.com.br>
2021-04-27 13:52:03 -0300

    Interface inicial & desenvolvimento pausado
    a lib pytube esta desatualizada em relação ao padrão regex do google para obter informações do link então o desenvolvimento vai ficar parado até ser atualizado

--- main.py
+++ main.py
@@ -1,57 +1,63 @@
-# import sys
-# import subprocess
+from PyQt5 import QtCore, QtGui, QtWidgets
+from interface import Ui_MainWindow
 from pytube import YouTube
 import ffmpeg
 from pytube.exceptions import VideoUnavailable, RegexMatchError
 
-
-class PytubeDownloader:
-    resolutions = {'1': [299, 140],
-                   '2': [136, 140],
-                   '3': [135, 140],
-                   '4': [134, 140],
-                   '5': [133, 140],
-                   '6': [160, 140]}  # The resolution match by it number and itag for audio and video
-
-    def __init__(self, url, pathvideo='./downloads/video.mp4', pathaudio='./downloads/audio.mp4'):
-        try:
-            self.video = YouTube(url)
-            self.pathvideo = ffmpeg.input(pathvideo)
-            self.pathaudio = ffmpeg.input(pathaudio)
-        except VideoUnavailable:
-            print('This video is unavailable')
-            exit()
-        except RegexMatchError:
-            print('Please enter a valid youtube video URL!')
-            exit()
-
-    def Summary(self):
-        print('Summary:')
-        print(f'Title: {self.video.title}')
-        print(f'Duration: {self.video.length / 60:.2f} minutes')
-        print(f'Rating: {self.video.rating:.2f}')
-        print(f'{self.video.views} views\n')
-        print('What resolution you want?')
-
-    def Download(self, command):
-        self.video.streams.get_by_itag(self.resolutions[command][0])\
-            .download(output_path='./downloads/', filename='video')
-        self.video.streams.get_by_itag(self.resolutions[command][1])\
-            .download(output_path='./downloads/', filename='audio')
-
-    def FFmpegConcat(self):
-        ffmpeg.concat(self.pathvideo, self.pathaudio, v=1, a=1).output(f'./downloads/{self.video.title}.mp4').run()
-        # Same FFmpeg process to join video and audio but using subprocess instead of ffmpeg-python
-        # subprocess.run(f"""ffmpeg -i self.pathvideo -i self.pathaudio
-        #              -c 'copy' './downloads/{self.video.title}.mp4'""")
-# ---------------------------------------------------------------------------------------------------------------------
+resolutions = []
+itags = []
 
 
-print('Paste the URL of the video:')
-# video = YouTube(input())
-# for i in video.streams.filter(file_extension='mp4'):
-#     print(i)
-run = PytubeDownloader(input())
-run.Summary()
-run.Download(input('1 - 1080p, 2 - 720p, 3 - 480p, 4 - 360, 5 - 240p, 6 - 144p : '))
-run.FFmpegConcat()
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        class status: status = True
+
+        def Link():
+            global video
+            resolutions.clear()
+            itags.clear()
+            try:
+                print(self.txtURL.toPlainText())
+                video = YouTube(str(self.txtURL.toPlainText()))
+            except VideoUnavailable:
+                print('This video is unavailable')
+            except RegexMatchError:
+                print('Please enter a valid youtube video URL!')
+            else:
+                Resolutions()
+
+        def Resolutions():
+            global resolutions
+            for i in video.streams:
+                i = str(i).split()
+                resolutions.append(i[3].strip('res="'))
+                itags.append(i[1].strip('itag="'))
+            for i in resolutions:
+                self.cbxResolutions.addItem(i)
+            Summary()
+
+        def Summary():
+            self.lblName.setText(video.title)
+            self.lblViews.setText(f'{self.video.views} views')
+            self.lblDuration.setText(f'Duration: {self.video.length / 60:.2f} minutes')
+
+        def Download(command):
+            self.video.streams.get_by_itag(self.resolutions[1][0]) \
+                .download(output_path='./downloads/', filename='video')
+            self.video.streams.get_by_itag(self.resolutions[command][1]) \
+                .download(output_path='./downloads/', filename='audio')
+
+        def FFmpegConcat():
+            ffmpeg.concat(self.pathvideo, self.pathaudio, v=1, a=1).output(f'./downloads/{self.video.title}.mp4').run()
+
+        self.psbCheck.clicked.connect(lambda: Link())
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    ui = MainWindow()
+    ui.show()
+    sys.exit(app.exec_())
